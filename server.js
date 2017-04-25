6f4b2dd22c59fed0bf5cb3fd56fb6692e0b1f66e
Dong Zhou <dz2714a@student.american.edu>
2017-04-25 16:43:34 -0400

    chatroom v1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Express initialization
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
@@ -8,3 +10,18 @@ app.get('/', function (request, response) {
 });
 
 app.listen(process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3003, function(){
+  console.log('listening on *:3003');
+});
