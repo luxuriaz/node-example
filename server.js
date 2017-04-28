150eba43f9abea0473d83957c15c4023df83a7eb
Dong Zhou <dz2714a@student.american.edu>
2017-04-28 16:33:35 -0400

    chatroom should be ok

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
-// Express initialization
-var express = require('express');
-var app = express();
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (request, response) {
-  response.set('Content-Type', 'text/html');
-  response.send('<p>Hey, it works!</p>');
-});
-
-app.listen(process.env.PORT || 3000);
-
 app.get('/', function(req, res){
+  res.set('Content-Type','text/html');
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
-http.listen(3003, function(){
-  console.log('listening on *:3003');
+
+http.listen(3009, function(){
+  console.log('listening on *:3009');
 });
